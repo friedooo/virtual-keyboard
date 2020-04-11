1c6abcd8f7f1cbdb912cc3d4f3f14fed48d4c3a0
Александр <54706207+friedooo@users.noreply.github.com>
2020-04-11 17:06:58 +0300

    feat: remove eslint comment

--- src/keys.js
+++ src/keys.js
@@ -1,4 +1,3 @@
-/* eslint-disable linebreak-style */
 
 const CODE = ['Backquote', 'Digit1', 'Digit2', 'Digit3', 'Digit4', 'Digit5',
   'Digit6', 'Digit7', 'Digit8', 'Digit9', 'Digit0', 'Minus', 'Equal', 'Backspace',
